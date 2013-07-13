3d541d706cf40dca0a3779dbba5fa5178765b8b8
Abhay Kumar Singh <abhayawesome@gmail.com>
2013-07-13 18:38:54 +0530

    The basic architecture is ready for phase1.
    Client.java and Server.java are to be modified in future
    commits so as to suit the actual application. Here the
    client sents message to server and server simply pretends
    to execute the message. .gitignore was also modified to
    ignore unnecessary file.

--- Client.java
+++ Client.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.net.*;
-
+import java.util.Scanner;
+import java.nio.charset.Charset;
 public class Client{
 	ObjectOutputStream output;
 	ObjectInputStream input;
 	String message = "";
+    Socket client;
 
+	/*
 	public static void main(String argsp[]){
 		Client app = new Client();
 		app.runClient();
@@ -53,4 +56,140 @@ public class Client{
 			io.printStackTrace();
 		}
 	}
+	*/
+
+	public static void main(String args[]){
+		
+		Client app = new Client();
+		app.mainLoop();
+	}
+	
+    int readFromUser(){
+        Scanner scan = new Scanner(System.in);
+        int option;
+        do{
+            try{
+                option = scan.nextInt();
+                break;          
+            }
+            catch(Exception e){
+                System.out.println("You can enter options 1, 2 or 3 only.");
+                scan.next();
+            }
+        }while(true);
+        return option;
+
+    }
+
+    void createConnection(String ip){
+
+        try {
+            client = new Socket( InetAddress.getByName(ip), 5000);
+            //Get input and output streams.
+            output = new ObjectOutputStream( client.getOutputStream());
+            output.flush();
+            input = new ObjectInputStream( client.getInputStream());
+
+
+        }
+
+        catch( EOFException eof ) {
+            System.out.println("....");
+        }
+        catch( IOException io) {
+            io.printStackTrace();
+        }
+
+    }
+
+    void closeConnection(){
+        try{
+            //Close connection.
+            System.out.println("Closing connection.");
+            output.close();
+            input.close();
+            client.close();
+        }
+        catch( IOException io) {
+            io.printStackTrace();
+        }
+    }
+
+    void executeOption(int option){
+        InputStream    fis;
+        BufferedReader br;
+        String         ip;
+        
+        try{
+            //Open file and read the ip's
+            fis = new FileInputStream("text.txt");
+            br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+            while ((ip = br.readLine()) != null) {
+                //For each ip create connection and send option value.
+                if(ip.trim().length()==0)
+                    continue;
+                createConnection(ip);
+                String message = Integer.toString(option) ;
+                output.writeObject(message);
+                output.flush();
+                   
+                
+                //Wait for acknowledgement
+                try{
+                    message = (String) input.readObject();
+                    System.out.println( message);
+                }
+                catch( ClassNotFoundException cnfex) {
+                    System.out.println("Unknown object type received");
+                }
+                //Close connection.
+                while(message == option + "done"){
+                    closeConnection();
+                }
+
+            }
+            //Close file.
+            br.close();
+            br = null;
+            fis = null;
+            //Inform about the completion of task
+            System.out.println("Task #" + option + " completed.");
+        }
+        catch(Exception e){
+            System.out.println("Excetions to be handled properly:(known)FileNotFound,IOExcep");
+        }
+    }
+
+	void mainLoop(){
+		
+        int option;
+        do{
+            System.out.println("Following are the options:");
+            System.out.println("1. Start platforms at machines with ip in the file.");
+            System.out.println("2. Kill all the platforms.");
+            System.out.println("3. Quit the program.");
+
+            option = readFromUser();
+            
+            switch(option) {
+                case 1:
+                    executeOption(1);
+                    break;
+                
+                case 2:
+                    executeOption(2);
+                    break;
+                
+                case 3:
+                    System.out.println("Quitting");
+                    break;
+                
+                default:
+                    System.out.println("This option was not provied");
+                    break;
+            }
+
+        }while(option != 3);
+	}
 }
+
--- Server.java
+++ Server.java
@@ -4,7 +4,8 @@ import java.net.*;
 public class Server{
 	ObjectOutputStream output;
 	ObjectInputStream input;
-
+    int option;
+    String message;
 	public static void main(String args[]){
 		Server app = new Server();
 		app.runServer();
@@ -18,38 +19,58 @@ public class Server{
 			//Create a ServerSocket.
 			server = new ServerSocket( 5000, 100);
 
-			while(true) {
-				//Wait for a connection.
-				connection = server.accept();
-				System.out.println("Connection received from" + connection.getInetAddress().getHostName());
+            while(true) {
+                //Wait for a connection.
+                connection = server.accept();
+                System.out.println("Connection received from" + connection.getInetAddress().getHostName());
 
-				//Get input and output streams.
-				output = new ObjectOutputStream( connection.getOutputStream());
-				output.flush();
-				input = new ObjectInputStream( connection.getInputStream());
-				System.out.println("Got I/O streams");
+                //Get input and output streams.
+                output = new ObjectOutputStream( connection.getOutputStream());
+                output.flush();
+                input = new ObjectInputStream( connection.getInputStream());
+                System.out.println("Got I/O streams");
 
-				//Process connection.
-				String message = "SERVER>>> Connection successful";
-				output.writeObject(message);
-				output.flush();
+                //Process connection.
+                //String message = "SERVER>>> Connection successful";
+                //output.writeObject(message);
+                //output.flush();
 
-				do {
-					try{
-						message = (String) input.readObject();
-						System.out.println( message);
-					}
-					catch( ClassNotFoundException cnfex) {
-						System.out.println("Unknown object type received");
-					}
-				}while( !message.equals("CLIENT>>> TERMINATE"));
-				
-				//Close connection.
-				System.out.println("User terminated connection");
-				output.close();
-				input.close();
-				connection.close();
-			}
+
+                try{
+                    message = (String) input.readObject();
+                    option = Integer.parseInt(message);
+                }
+                catch( ClassNotFoundException cnfex) {
+                    System.out.println("Unknown object type received");
+                }
+
+                switch(option){
+                    case 1:
+                        System.out.println("First executed.");
+                        message = "1done";
+                        output.writeObject(message);
+                        output.flush();
+                        break;
+
+                    case 2:
+                        System.out.println("Second executed.");
+                        message = "2done";
+                        output.writeObject(message);
+                        output.flush();
+                        break;
+
+                    default:
+                        System.out.println("Code should not reach default.");
+                        break;
+                        
+                }
+                //Close connection.
+                //System.out.println("User terminated connection");
+                output.close();
+                input.close();
+                connection.close();
+                System.out.println("Closing this connection.");
+            }
 		}
 		catch( EOFException eof ) {
 			System.out.println("Client terminated connection");
